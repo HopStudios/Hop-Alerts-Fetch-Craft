e2b9eb3b221c18a24bf2e8bf65dc9ce15ac0672a
Louis DEKEISTER <louis@hopstudios.com>
2017-04-17 15:12:00 -0700

    update main helper with better code

--- hopalertsfetch/helpers/Main_Helper.php
+++ hopalertsfetch/helpers/Main_Helper.php
@@ -4,7 +4,7 @@ namespace Craft;
 
 class Main_Helper
 {
-	public static function createEntry($sectionHandle, $author_id, $title, $type, $type_handle, $custom_id, $custom_id_handle, $content, $content_handle, $timestamp = NULL)
+	public static function createAlertEntry($sectionHandle, $author_id, $title, $type, $type_handle, $custom_id, $custom_id_handle, $content, $content_handle, $timestamp = NULL)
 	{
 		$entry = new EntryModel();
 		$entry->sectionId = craft()->sections->getSectionByHandle($sectionHandle)->id;
@@ -24,7 +24,43 @@ class Main_Helper
 		$entry->getContent()->{$custom_id_handle}	= $custom_id;
 		$entry->getContent()->{$content_handle}		= $content;
 
-		$entry->getContent()->title = $title;
+		if (craft()->entries->saveEntry($entry))
+		{
+			
+		}
+		else
+		{
+
+			HopAlertsFetchPlugin::log('Tried to create an Alert entry but saving into DB failed: '.$entry->getErrors() , LogLevel::Error);
+		}
+	}
+
+	public static function createEntry($sectionHandle, $author_id, $title, $fields, $timestamp = NULL)
+	{
+		if (!is_array($fields))
+		{
+			throw new Exception('Main_Helper::createEntry() - 4th parameter must be an array.');
+		}
+
+		$entry = new EntryModel();
+		$entry->sectionId = craft()->sections->getSectionByHandle($sectionHandle)->id;
+
+		$postDate = new DateTime();
+		if ($timestamp)
+		{
+			$postDate->setTimestamp($timestamp);
+		}
+		$postDate = DateTime::createFromString($postDate, craft()->timezone);
+
+		$entry->enabled								= TRUE;
+		$entry->authorId 							= $author_id;
+		$entry->postDate							= $postDate;
+		$entry->getContent()->title					= $title;
+
+		foreach($fields as $field_name => $field_value)
+		{
+			$entry->getContent()->{$field_name} = $field_value;
+		}
 
 		if (craft()->entries->saveEntry($entry))
 		{

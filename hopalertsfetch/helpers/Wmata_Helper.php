b60cff713f59f4474c77385e2a9e8430b50dc6ea
Louis DEKEISTER <louis@hopstudios.com>
2017-04-17 16:27:19 -0700

    setup logic to update bus and rail updates

--- hopalertsfetch/helpers/Wmata_Helper.php
+++ hopalertsfetch/helpers/Wmata_Helper.php
@@ -13,24 +13,39 @@ class Wmata_Helper
 	*/
 
 	private $wmata_api;
+	private $active_section_handle;
+	private $inactive_section_handle;
+	private $author_id;
+	private $type_handle;
+	private $custom_id_handle;
+	private $content_handle;
+	private $refreshDelay;
+	private $lastUpdateBus;
+	private $lastUpdateRail;
 
-	public function __construct($wmata_api_key)
+	public function __construct()
 	{
-		$wmata_api = new Wmata_API($wmata_api_key);
-		$this->wmata_api = $wmata_api;
+		$settings						= craft()->plugins->getPlugin('HopAlertsFetch')->getSettings();
+		$this->wmata_api				= new Wmata_API($settings->wmata_api_key);
+		$this->active_section_handle	= $settings->alerts_section_handle;
+		$this->inactive_section_handle	= $settings->alerts_expired_section_handle;
+		$this->author_id				= $settings->author_id;
+		$this->type_handle				= $settings->field_handle_alert_type;
+		$this->custom_id_handle			= $settings->field_handle_alert_ext_id;
+		$this->content_handle			= $settings->field_handle_alert_body;
+		$this->refreshDelay				= $settings->time_refresh;
+		$this->lastUpdateBus			= $settings->last_update_bus;
+		$this->lastUpdateRail			= $settings->last_update_rail;
 	}
 
 	public function update_alerts()
 	{
-		$refresh_delay = HAF_settings_helper::get_time_refresh();
-		$last_bus_update = HAF_settings_helper::get_setting('last_update_bus');
-		$last_rail_update = HAF_settings_helper::get_setting('last_update_rail');
-		$now = ee()->localize->now;
+		$dt = new DateTime();
+		$now = $dt->getTimestamp();
 
-		if ($last_bus_update != NULL && $last_bus_update != '')
+		if ($this->lastUpdateBus != NULL && $this->lastUpdateBus != '')
 		{
-			$last_bus_update = intval($last_bus_update);
-			if ($now - $last_bus_update > $refresh_delay)
+			if ($now - $this->lastUpdateBus > $this->refreshDelay)
 			{
 				$this->_update_bus_alerts();
 			}
@@ -40,10 +55,9 @@ class Wmata_Helper
 			$this->_update_bus_alerts();
 		}
 
-		if ($last_rail_update != NULL && $last_rail_update != '')
+		if ($this->lastUpdateRail != NULL && $this->lastUpdateRail != '')
 		{
-			$last_rail_update = intval($last_rail_update);
-			if ($now - $last_rail_update > $refresh_delay)
+			if ($now - $this->lastUpdateRail > $this->refreshDelay)
 			{
 				$this->_update_rail_alerts();
 			}
@@ -57,101 +71,137 @@ class Wmata_Helper
 
 	private function _update_bus_alerts()
 	{
-		ee()->load->library('logger');
-		$channel_id = HAF_settings_helper::get_alerts_channel_id();
-		$channel_id_expired = HAF_settings_helper::get_alerts_channel_id_expired();
-		$field_id_type = HAF_settings_helper::get_field_id_alert_type();
-		$field_id_ext_id = HAF_settings_helper::get_field_id_alert_ext_id();
-		$entries_res = ee('Model')->get('ChannelEntry')
-			->filter('channel_id', $channel_id)
-			->filter('status', 'IN', array('open'))
-			->filter('field_id_'.$field_id_type, 'bus')
-			->order('entry_date', 'DESC')
-			->all();
 
+		// $channel_id = HAF_settings_helper::get_alerts_channel_id();
+		// $channel_id_expired = HAF_settings_helper::get_alerts_channel_id_expired();
+		// $field_id_type = HAF_settings_helper::get_field_id_alert_type();
+		// $field_id_ext_id = HAF_settings_helper::get_field_id_alert_ext_id();
+		// $entries_res = ee('Model')->get('ChannelEntry')
+		// 	->filter('channel_id', $channel_id)
+		// 	->filter('status', 'IN', array('open'))
+		// 	->filter('field_id_'.$field_id_type, 'bus')
+		// 	->order('entry_date', 'DESC')
+		// 	->all();
+
+		$criteria = craft()->elements->getCriteria(ElementType::Entry);
+		$criteria->section = $this->active_section_handle;
+		$criteria->status = null; // Get all entries in this section
+		$criteria->order = 'postDate desc';
+		$criteria->limit = 500000; // This is to make sure we got max results possible
+
+		$results = $criteria->find(array($this->type_handle => 'bus'));
 		$entries = array();
-		foreach ($entries_res as $entry_res)
+		foreach ($results as $entry_res)
 		{
-			$entries[$entry_res->{'field_id_'.$field_id_ext_id}] = $entry_res;
+			// $entries[$entry_res->{'field_id_'.$field_id_ext_id}] = $entry_res;
+			$entries[$entry_res->{$this->custom_id_handle}] = $entry_res;
 		}
+
+		// echo 'update BUS';
 
 		$result = json_decode($this->wmata_api->get_bus_incidents());
 		if ($result == NULL)
 		{
-			if (HAF_settings_helper::get_debug())
-			{
-				ee()->logger->developer('HAF: json received for buses is not valid');
-			}
+			HopAlertsFetchPlugin::log('json received for buses is not valid', LogLevel::Error);
 			return;
 		}
 
-		if (isset($result->BusIncidents) && is_array($result->BusIncidents) && count($result->BusIncidents) > 0)
+		if (isset($result->BusIncidents) && is_array($result->BusIncidents))
 		{
-			foreach ($result->BusIncidents as $busIncident)
+			if (count($result->BusIncidents) > 0)
 			{
-				if (array_key_exists($busIncident->IncidentID, $entries))
+				foreach ($result->BusIncidents as $busIncident)
 				{
-					// nothing to do, entry is present and alert is still going
-					// Remove it from the entries array
-					unset($entries[$busIncident->IncidentID]);
-				}
-				else
-				{
-					$title = 'Bus '.$busIncident->IncidentType.' on ';
-					foreach ($busIncident->RoutesAffected as $route)
+					if (array_key_exists($busIncident->IncidentID, $entries))
 					{
-						$title .= $route.', ';
+						// nothing to do, entry is present and alert is still going
+						// Remove it from the entries array
+						unset($entries[$busIncident->IncidentID]);
 					}
-					$title = substr($title, 0, -2);
+					else
+					{
+						$title = 'Bus '.$busIncident->IncidentType.' on ';
+						foreach ($busIncident->RoutesAffected as $route)
+						{
+							$title .= $route.', ';
+						}
+						$title = substr($title, 0, -2);
 
-					// DateTime is in Eastern Standard Time EST, we need to convert it to GMT/UTC to store it
-					$dt = new DateTime($busIncident->DateUpdated, new DateTimeZone('EST'));
-					$dt->setTimezone(new DateTimeZone('UTC'));
+						// DateTime is in Eastern Standard Time EST, we need to convert it to GMT/UTC to store it
+						$dt = new DateTime($busIncident->DateUpdated, new \DateTimeZone('EST'));
+						$dt->setTimezone(new \DateTimeZone('UTC'));
 
-					// Create a new entry with alert data
-					HAF_helper::create_alert_entry($title, 'bus', $busIncident->IncidentID, $busIncident->Description, $dt->format('U'));
+						// Create a new entry with alert data 
+						// $sectionHandle, $author_id, $title, $type, $type_handle, $custom_id, $custom_id_handle, $content, $content_handle, $timestamp = NULL
+						Main_Helper::createAlertEntry(
+							$this->active_section_handle,
+							$this->author_id,
+							$title,
+							'bus',
+							$this->type_handle,
+							$busIncident->IncidentID,
+							$this->custom_id_handle,
+							$busIncident->Description,
+							$this->content_handle,
+							$dt->format('U')
+						);
+					}
+				}
+
+				$inactive_section_id = craft()->sections->getSectionByHandle($this->inactive_section_handle)->id;
+				foreach ($entries as $entry)
+				{
+					// Close remaining entries, as that means they're not active anymore
+					// $entry->status = 'closed'; // Switch entry to disabled ?
+					$entry->sectionId = $inactive_section_id;
+					$success = craft()->entries->saveEntry($entry);
 				}
 			}
 
-			foreach ($entries as $entry)
-			{
-				// Close remaining entries, as that means they're not going anymore
-				$entry->status = 'closed';
-				$entry->channel_id = $channel_id_expired;
-				$entry->save();
-			}
-
-			HAF_settings_helper::save_setting('last_update_bus', ee()->localize->now);
+			$dt = new DateTime();
+			$now = $dt->getTimestamp();
+			$haf = craft()->plugins->getPlugin( 'HopAlertsFetch' );
+			craft()->plugins->savePluginSettings($haf, array('last_update_bus' => $now));
+		}
+		else if (isset($result->message))
+		{
+			HopAlertsFetchPlugin::log('Error when retrieving BUS alerts: "'.$result->message.'"', LogLevel::Error);
 		}
 	}
 
 	private function _update_rail_alerts()
 	{
-		ee()->load->library('logger');
-		$channel_id = HAF_settings_helper::get_alerts_channel_id();
-		$channel_id_expired = HAF_settings_helper::get_alerts_channel_id_expired();
-		$field_id_type = HAF_settings_helper::get_field_id_alert_type();
-		$field_id_ext_id = HAF_settings_helper::get_field_id_alert_ext_id();
-		$entries_res = ee('Model')->get('ChannelEntry')
-			->filter('channel_id', $channel_id)
-			->filter('status', 'IN', array('open'))
-			->filter('field_id_'.$field_id_type, 'rail')
-			->order('entry_date', 'DESC')
-			->all();
+		// $channel_id = HAF_settings_helper::get_alerts_channel_id();
+		// $channel_id_expired = HAF_settings_helper::get_alerts_channel_id_expired();
+		// $field_id_type = HAF_settings_helper::get_field_id_alert_type();
+		// $field_id_ext_id = HAF_settings_helper::get_field_id_alert_ext_id();
+		// $entries_res = ee('Model')->get('ChannelEntry')
+		// 	->filter('channel_id', $channel_id)
+		// 	->filter('status', 'IN', array('open'))
+		// 	->filter('field_id_'.$field_id_type, 'rail')
+		// 	->order('entry_date', 'DESC')
+		// 	->all();
 
+		$criteria = craft()->elements->getCriteria(ElementType::Entry);
+		$criteria->section = $this->active_section_handle;
+		$criteria->status = null; // Get all entries in this section
+		$criteria->order = 'postDate desc';
+		$criteria->limit = 500000; // This is to make sure we got max results possible
+
+		$results = $criteria->find(array($this->type_handle => 'rail'));
 		$entries = array();
-		foreach ($entries_res as $entry_res)
+		foreach ($results as $entry_res)
 		{
-			$entries[$entry_res->{'field_id_'.$field_id_ext_id}] = $entry_res;
+			// $entries[$entry_res->{'field_id_'.$field_id_ext_id}] = $entry_res;
+			$entries[$entry_res->{$this->custom_id_handle}] = $entry_res;
 		}
+
+		// echo 'update Rail';
 
 		$result = json_decode($this->wmata_api->get_rail_incidents());
 		if ($result == NULL)
 		{
-			if (HAF_settings_helper::get_debug())
-			{
-				ee()->logger->developer('HAF: json received for rail is not valid');
-			}
+			HopAlertsFetchPlugin::log('json received for buses is not valid', LogLevel::Error);
 			return;
 		}
 
@@ -179,23 +229,42 @@ class Wmata_Helper
 					$title = substr($title, 0, -2);
 
 					// DateTime is in Eastern Standard Time EST, we need to convert it to GMT/UTC to store it
-					$dt = new DateTime($railIncident->DateUpdated, new DateTimeZone('EST'));
-					$dt->setTimezone(new DateTimeZone('UTC'));
+					$dt = new DateTime($railIncident->DateUpdated, new \DateTimeZone('EST'));
+					$dt->setTimezone(new \DateTimeZone('UTC'));
 
 					// Create a new entry with alert data
-					HAF_helper::create_alert_entry($title, 'rail', $railIncident->IncidentID, $railIncident->Description, $dt->format('U'));
+					// HAF_helper::create_alert_entry($title, 'rail', $railIncident->IncidentID, $railIncident->Description, $dt->format('U'));
+					Main_Helper::createAlertEntry(
+						$this->active_section_handle,
+						$this->author_id,
+						$title,
+						'rail',
+						$this->type_handle,
+						$railIncident->IncidentID,
+						$this->custom_id_handle,
+						$railIncident->Description,
+						$this->content_handle,
+						$dt->format('U')
+					);
 				}
 			}
 
+			$inactive_section_id = craft()->sections->getSectionByHandle($this->inactive_section_handle)->id;
 			foreach ($entries as $entry)
 			{
 				// Close remaining entries, as that means they're not going anymore
-				$entry->status = 'closed';
-				$entry->channel_id = $channel_id_expired;
-				$entry->save();
+				$entry->sectionId = $inactive_section_id;
+				$success = craft()->entries->saveEntry($entry);
 			}
 
-			HAF_settings_helper::save_setting('last_update_rail', ee()->localize->now);
+			$dt = new DateTime();
+			$now = $dt->getTimestamp();
+			$haf = craft()->plugins->getPlugin( 'HopAlertsFetch' );
+			craft()->plugins->savePluginSettings($haf, array('last_update_bus' => $now));
+		}
+		else if (isset($result->message))
+		{
+			HopAlertsFetchPlugin::log('Error when retrieving RAIL alerts: "'.$result->message.'"', LogLevel::Error);
 		}
 	}
 }
